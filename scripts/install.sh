e1c35228d013b716c0ee823db3538fd2998abf66
oscarmooregcp <oscarmoore@google.com>
2025-11-06 18:45:53 +0000

    install.sh changed to new repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -85,7 +85,7 @@ function parse_command_line_arguments() {
 
 # Function to clone and checkout repository
 function clone_and_checkout_repository() {
-  TARGET_REPO_URL="https://github.com/oscarmooregcp/crmint_olbm/.git"
+  TARGET_REPO_URL="https://github.com/oscarmooregcp/crmint_olbm.git"
   TARGET_REPO_NAME="crmint_olbm"
   CLONE_DIR="$HOME/$TARGET_REPO_NAME"
 
